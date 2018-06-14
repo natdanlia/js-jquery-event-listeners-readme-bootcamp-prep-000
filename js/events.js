755ce111e67829f3e72fe53283d492e4498d4bbd
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-06-14 15:58:07 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,6 +21,8 @@ function frameIt(){
 
 function pressIt(){
   $('form input:first-child').on('load', function(key){
-    if(key.which == )
+    if(key.which == 71){
+      alert('G has been pressed!')
+    }
   })
 }
