956e74f0c911755d373b099300e0246a9aff4b76
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-06-14 16:14:21 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,44 @@
 
 $(document).ready(function(){
 
-// call functions here
-
+ 
+ getIt();
+ frameIt();
+ pressIt();
+ submitIt();
 });
+
+function getIt(){
+  $('p').on('click', function(){
+    alert ('hey!')
+  })
+}
+
+function frameIt(){
+ $('img').on('load', function(){
+    $("img").addClass("tasty")
+  })
+}
+
+function pressIt(){
+  $('form input:first-child').on('keydown', function(key){
+    if(key.which == 71){
+      alert('G has been pressed!')
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+
+
+
+
+
+
+
+
